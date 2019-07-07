0590fda769734b7dbeef7df6c2f1dc9f9c325edd
ZhangFei <yunfei90@hotmail.com>
2019-07-07 09:02:09 +0800

    初始导入

---
+++ src/main/java/com/zhangfei/calc/MycalcApplication.java
@@ -0,0 +1,13 @@
+package com.zhangfei.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycalcApplication.class, args);
+	}
+
+}
